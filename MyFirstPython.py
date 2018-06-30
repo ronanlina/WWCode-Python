5bb0716916e9e6106aa6a067de289619dba2098f
Ronan Lina <35568096+ronanlina@users.noreply.github.com>
2018-06-30 16:31:26 +0800

    Add files via upload

---
+++ MyFirstPython.py
@@ -0,0 +1,21 @@
+# Basic example on how to print
+# text using different approach in
+# Python.
+
+organization = 'WomenWhoCode!'
+
+# You can print it right away!
+print('Hello WomanWhoCode!')
+
+# Example of string concatenation
+print('Hello ' + organization)
+
+# Using .format() function to attach a string
+print('Hello {}'.format(organization))
+# print(f'Hello {organization}')  # Can use this format with Python3.6
+
+# First, replace the following text with your name
+your_name = 'Ronan'
+
+# Then, uncomment the next line by removing the "#" sign
+#print('{} loves Python!'.format(your_name))
